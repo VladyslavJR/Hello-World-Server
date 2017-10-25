61de34f9ddc2a6ac83aff6f4573cc8da14f34ff6
Vladislav_JR <vladislav.glushuk@gmail.com>
2017-10-25 22:40:17 +0300

    - Delete the previous django site. - Add support for cmd - Add a sample function to get time to MainPage Request Handler - Add a sample Request Handler (SamplePage)

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 import webapp2
+import datetime
+
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        self.response.write('Time: %s' % self.time_get())
+
+    def time_get(self):
+        return 'Now: %s' % datetime.datetime.now().__str__()
+
+
+class SamplePage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text\plain'
+        self.response.write("Hey there!")
 
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/hey/', SamplePage),
 ], debug=True)
+
+def main():
+    from paste import httpserver
+    httpserver.serve(app, host='127.0.0.1', port='8000')
+
+if __name__ == '__main__':
+    main()
