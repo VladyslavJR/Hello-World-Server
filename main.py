b36360591a630efcc90f4d32927c04bd025c32e8
Vladislav_JR <vladislav.glushuk@gmail.com>
2017-10-27 21:01:19 +0300

    Change the form in MainPage get()

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class MainPage(webapp2.RequestHandler):
         self.response.out.write("""
                   <html>
                     <body>
-                      <form action="add_post" method="post">
+                      <form method="post">
                         <div><textarea name="content" rows="3" cols="60"></textarea></div>
                         <div><input type="submit" value="Sign Guestbook"></div>
                       </form>
